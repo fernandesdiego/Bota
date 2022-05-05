054ec144d9d03b6e5ca498fffa30ac69ad147b42
Diego Fernandes <fernandes.diego_@hotmail.com>
2022-05-05 16:32:53 -0300

    Add Poll Service

--- Bota/Modules/InteractionGeneralModule.cs
+++ Bota/Modules/InteractionGeneralModule.cs
@@ -1,4 +1,5 @@
 ﻿using Bota.Services;
+using Discord;
 using Discord.Interactions;
 using System;
 using System.Collections.Generic;
@@ -12,10 +13,12 @@ namespace Bota.Modules
     {
         public InteractionService InteractionCommands { get; set; }
 
+        private readonly PollService _pollService;
 
-        public InteractionGeneralModule(InteractionService interaction)
+        public InteractionGeneralModule(InteractionService interaction, PollService pollService)
         {
             InteractionCommands = interaction;
+            _pollService = pollService;
         }
 
         [SlashCommand("ping", "Retorna pong")]
@@ -41,6 +44,21 @@ namespace Bota.Modules
             }
 
             await Context.Interaction.RespondAsync(info);
+        }
+
+        [SlashCommand("votacao", "Cria uma votação")]
+        public async Task Poll([Summary("questão", "Qual a questão da sua votação?")] string question, [Summary("duracao", "Quanto tempo vai durar sua votação")] int duration, [Summary("opcoes", "As opções disponíveis separadas por virgula")] string options, [Summary("descrição", "Descrição opcional da sua votação")] string description = null)
+        {
+            await _pollService.CreatePoll(Context, question, duration, options, description);
+        }
+
+        [SlashCommand("teste", "testando")]
+        public async Task Test()
+        {
+            var eb = new EmbedBuilder().WithTitle("EmbedTest");
+            var cb = new ComponentBuilder().WithButton("ComponentButton", Guid.NewGuid().ToString());
+            cb.WithButton("SecondComponentButton", Guid.NewGuid().ToString());
+            await Context.Interaction.RespondAsync(embed: eb.Build(), components: cb.Build());
         }
     }
 }
---
+++ Bota/Services/PollService.cs
@@ -0,0 +1,137 @@
+﻿using Bota.Models;
+using Discord;
+using Discord.Addons.Hosting;
+using Discord.Commands;
+using Discord.Interactions;
+using Discord.WebSocket;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bota.Services.Extensions;
+
+namespace Bota.Services
+{
+    public class PollService
+    {
+        private readonly IServiceProvider _services;
+        private readonly InteractionService _commands;
+        private readonly DiscordSocketClient _client;
+        private static List<Poll> Polls = new List<Poll>();
+
+        public PollService(IServiceProvider services, InteractionService commands, DiscordSocketClient client)
+        {
+            _services = services;
+            _commands = commands;
+            _client = client;
+            _client.ButtonExecuted += OnButtonExecuted;
+        }
+
+        private async Task OnButtonExecuted(SocketMessageComponent arg)
+        {
+            Poll clickedPoll = Polls.Find(p => p.MessageId == arg.Message.Id);
+            if (clickedPoll == null)
+            {
+                await arg.RespondAsync("Erro ao computar voto.", ephemeral: true);
+                return;
+            }
+
+            if (clickedPoll.Voters.Any(x => x.Id == arg.User.Id))
+            {
+                await arg.RespondAsync("Você já votou.", ephemeral: true);
+                return;
+            }
+
+            var clickedButton = clickedPoll.Options.Keys.First(x => x.CustomId == arg.Data.CustomId);
+
+            clickedPoll.Options[clickedButton]++;
+            clickedPoll.Voters.Add(arg.User);
+
+            var (embed, messageComponent) = BuildPollMessage(clickedPoll);
+
+            await arg.UpdateAsync(m =>
+            {
+                m.Embed = embed;
+                m.Components = messageComponent;
+            });
+        }
+
+        public async Task CreatePoll(SocketInteractionContext commandContext, string question, int duration, string options, string description = null)
+        {
+            var optionStrings = options.Split(",", StringSplitOptions.TrimEntries);
+
+            var btnDictionary = new Dictionary<ButtonComponent, int>();
+
+            foreach (var option in optionStrings)
+            {
+                var b = new ButtonBuilder
+                {
+                    Label = option,
+                    CustomId = Guid.NewGuid().ToString(),
+                    Style = ButtonStyle.Primary,
+                };
+                btnDictionary.Add(b.Build(), 0);
+            }
+
+            var poll = new Poll(question, btnDictionary, DateTime.Now, duration, commandContext.Channel, description);
+
+            poll.PollEnded += OnPollEnded;
+
+            var pollMessage = BuildPollMessage(poll);
+
+            await commandContext.Interaction.RespondAsync(embed: pollMessage.embed, components: pollMessage.messageComponent);
+            var response = await commandContext.Interaction.GetOriginalResponseAsync();
+
+            poll.MessageId = response.Id;
+
+            Polls.Add(poll);
+        }
+
+        private static (Embed embed, MessageComponent messageComponent) BuildPollMessage(Poll poll)
+        {
+            var embedBuilder = new EmbedBuilder()
+                .WithTitle($"📊 {poll.Question}")
+                .WithDescription(poll.Description)
+                .WithFooter($"Enquete termina em: {poll.StartTime.AddMinutes(poll.Duration)}");
+
+            var totalVotes = poll.Options.Values.Sum();
+
+            foreach (var option in poll.Options)
+            {
+                var optionPercent = totalVotes == 0 ? "0.00 %" : ((float)option.Value / (float)totalVotes).ToString("P");
+
+                embedBuilder.AddField($"🔹 {option.Key.Label} - {option.Value} ({optionPercent})", "\u200b");
+            }
+
+            var components = new ComponentBuilder().FromButtons(poll.Options.Keys);
+
+            return (embedBuilder.Build(), components.Build());
+        }
+
+        private void UpdatePoll(SocketInteractionContext context, SocketMessageComponent component)
+        {
+
+        }
+
+        private async void OnPollEnded(object source, PollEventArgs args)
+        {
+            var message = await args.Poll.TextChannel.GetMessageAsync(args.Poll.MessageId);
+
+            var embed = message.Embeds.First() as Embed;
+
+            var mp = new MessageProperties()
+            {
+                Embed = embed,
+                Components = new ComponentBuilder().Build(),
+                Content = "Enquete encerrada",
+            };
+
+            await args.Poll.TextChannel.ModifyMessageAsync(message.Id, x => x.Components = new ComponentBuilder().Build());
+
+            Polls.Remove(args.Poll);
+        }
+    }
+}
